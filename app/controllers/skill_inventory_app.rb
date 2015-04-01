58e322b04e6f00f5e8e68e201c414e028dad26fa
Mihir Parikh <mihir787@gmail.com>
2015-03-31 19:49:22 -0600

    skillz

---
+++ app/controllers/skill_inventory_app.rb
@@ -0,0 +1,40 @@
+require 'models/skill_inventory'
+
+class SkillInventoryApp < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
+
+  get '/skills' do
+    @skills = SkillInventory.all
+    erb :index
+  end
+
+  get '/skill/new' do
+    erb :new
+  end
+
+  post '/skills' do
+    SkillInventory.create(params[:skill])
+    redirect '/skills'
+  end
+
+  get '/skills/:id' do |id|
+    @skill = SkillInventory.find(id.to_i)
+    erb :show
+  end
+
+  get '/skills/:id/edit' do |id|
+    @skill = SkillInventory.find(id.to_i)
+    erb :edit
+  end
+
+  put '/skills/:id' do |id|
+    SkillInventory.update(id.to_i, params[:skill])
+    redirect "/skills/#{id}"
+  end
+
+  delete '/skills/:id' do |id|
+    SkillInventory.destroy(id.to_i)
+    redirect "/skills"
+  end
+end
